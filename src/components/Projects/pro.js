25260d27aa07c885c79bc7b5f0bbce3587c1aef0
W-Software-Solutions <154895656+W-Software-Solutions@users.noreply.github.com>
2024-10-25 20:56:25 +0530

    Add files via upload

---
+++ src/components/Projects/pro.js
@@ -0,0 +1,52 @@
+import p1 from "/projects/1.png";
+import p2 from "/projects/urlshort.png";
+import p3 from "/projects/passgen.png";
+import p4 from "/projects/2.png";
+import p5 from "/projects/personalport.png";
+
+export const PROJECTS = [
+  {
+    title: "Movieplex",
+    image: p1,
+    description:
+      "amazing website UI and comfortable UX with abilty to choose what u want to eat and add it in shopping cart and order functionality",
+    github: "https://github.com/Wasif-Ansari/Movieplex",
+    live: "https://w-movieplex.vercel.app/",
+  },
+  {
+    title: "URL Shortner",
+    image: p2,
+    description:
+      " an application for manging tasks and projects with fully crud experience",
+    github:
+      "https://github.com/Wasif-Ansari/URL-Shortner-MERN",
+    live: "https://w-url-shortner.netlify.app/",
+  },
+  {
+    title: "Password Generator",
+    image: p3,
+    description:
+      " mini e-commerce website with features like product listing and shopping cart ",
+    github:
+      "https://github.com/Wasif-Ansari/password_generator.github.io",
+    live: "https://wasif-ansari.github.io/password_generator.github.io/",
+  },
+  {
+    title: "Personal Portfolio",
+    image: p5,
+    description:
+      " a creative website that asks you to your location and provide to you the nearest good place to go",
+    github:
+      "https://github.com/Wasif-Ansari/Wasif-Ansari.github.io",
+    live: "https://wasif-ansari.github.io/",
+  },
+  {
+    title: "W Software Solutions",
+    image: p4,
+    description:
+      "A quiz application that can test your experiance in react with a timer for each question and fully result details at the end",
+    github: "https://github.com/Wasif-Ansari/W-Software-Solutions",
+    live: "https://w-softwares.vercel.app/",
+  },
+];
+
